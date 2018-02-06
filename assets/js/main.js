bf498839e01a249727c7dabe63a7fd7eca00fa8e
Jeffrey <jf2978@columbia.edu>
2018-02-07 00:47:22 +0100

    Additional prepano.html margin formatting + last collage added

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,15 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-
+/*$(function() {
+	$('#a').hover(function() {
+	  $('#b').css('background-color', 'yellow');
+	}, function() {
+	  // on mouseout, reset the background colour
+	  $('#b').css('background-color', '');
+	});
+  });
+*/
 (function($) {
 
 	skel.breakpoints({
