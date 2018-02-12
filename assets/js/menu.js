fe390a2e142c64d65f17e6e811b1b8f6726077be
Jeffrey <jf2978@columbia.edu>
2018-02-12 17:45:08 +0100

    fixed pano blue margin + start screen (wasn't hiding other sections before)

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,10 +4,11 @@ function(n) {
  constants: {
  sectionTemplate: '.section-template',
  contentContainer: '#wrapper',
- startSectionMenuItem: '#welcome-menu',
- startSection: '#welcome-menu'
+ startSectionMenuItem: '#intro',
+ startSection: '#intro'
  },
 
+ // looks for all imported html files and loads them into the #wrapper div so it can be found
  importSectionsToDOM: function() {
     const links = document.querySelectorAll('link[rel="import"]');
     
@@ -18,24 +19,32 @@ function(n) {
     })
  },
 
+ // Listens to menu click event: if the clicked, checks if fired event is
+ // of our custom data-section type (dataset accesses any data- attribute and we use 
+ // .keyname to then retrieve the value of the attribute with data-keyname)
  setMenuOnClickEvent: function () {
     document.body.addEventListener('click', function (event) {
-    if (event.target.dataset.section) {
+    if (event.target.dataset.section) { // returns intro, prepano, pano, etc.
         navigation.menu.hideAllSections();
         navigation.menu.showSection(event);
        }
     })
  },
 
+ // Uses jquery to directly show section by appending # to keyname
  showSection: function(event) {
     const sectionId = event.target.dataset.section;
-    $('#' + sectionId).show();
-    $('#' + sectionId + ' section').show();
+    console.log('#' + sectionId + ' showed');
+    $('#' + sectionId).show(); // displays entire html with section id
+    $('#' + sectionId + ' section').show(); // displays section element with that id
  },
 
  showStartSection: function() {
     $(this.constants.startSectionMenuItem).click();
+    navigation.menu.hideAllSections();
+    console.log(this.constants.startSectionMenuItem + ' clicked');
     $(this.constants.startSection).show();
+    console.log(this.constants.startSection + ' showed');
     $(this.constants.startSection + ' section').show();
  },
 
